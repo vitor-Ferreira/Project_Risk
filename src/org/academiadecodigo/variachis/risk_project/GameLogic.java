a44dcc464060d72dd467a582cb3bc58bf8e23812
Vitor Ferreira <vitor_jmf_15@hotmail.com>
2018-10-05 17:27:28 +0100

    Start to work on the game,created method Round and implemented the Board(i)

--- src/org/academiadecodigo/variachis/risk_project/GameLogic.java
+++ src/org/academiadecodigo/variachis/risk_project/GameLogic.java
@@ -1,84 +1,91 @@
 package org.academiadecodigo.variachis.risk_project;
 
-public class GameLogic {
+public class GameLogic implements Interface_Board {
+
+
     private Player p1;
-
-
-    private Territory[] territoriesArray;
     private Player p2;
-
-    public GameLogic() {
-        territoriesArray = new Territory[]{
-               // new Territory(0, "cona"),
-                //new Territory(1, "fodase"),
-                //new Territory(2, "pissa"),
-        };
-     //   p2 = new Player("Blue", 20, territoriesArray[0]);
-    }
+    private Board board;
 
 
-    public Player getP2() {
-        return p2;
-    }
+    public void round(Player player) {
 
-    public void setTerritoriesArray(Territory t, Territory q, Territory r) {
+        //Phase 1 - increment, uses the method increment from the board
+        //to increment the troops in territories that have the player that is playing this round
+        board.increment(player);
 
-        territoriesArray[0]=t;//error i want to c
-        territoriesArray[1]=q;
-        territoriesArray[2]=r;
-        p2 = new Player("Blue", 20, territoriesArray[0]);
 
-    }
-    //Note
-    /*
-                private Player player1;
-                private Player player2;
-                private Territory head;
+        //gets the territory (movement) that the player wants to attack.
+        Territory movement = p1.move();
 
-                public GameLogic(Player player1, Player player2) {
-                    this.player1=player1;
-                    this.player2=player2;
-                    this.head.getNext();
-                }
+        //check if the movement is allowed
+        if(!board.allowsMoviment(movement)){
 
-                public
-
-            */
-    public void moveToTerritory(Player p, Movement m) {//allows player to move / if the target territory belongs to him doesnt trigger the attack method
-        /*switch (-----receive an input----) {
-            case 0:
-                getPos().subSetCol();
-                break;
-            case 1:
-                getPos().addSetCol();
-                break;
-            case 2:
-                getPos().addSetRow();
-                break;
-            case 3:
-                getPos().subSetRow();
-                break;
-        }*/
-        switch (m) {
-            case UP:
-                p.setCurrentTerritory(territoriesArray[1]);
-            case DOWN:
-                p.setCurrentTerritory(territoriesArray[0]);
-            case LEFT:
-                p.setCurrentTerritory(territoriesArray[2]);
+            //if not allowed return choose new movement
+            //needs to keep chacking if new move is allowed...
         }
 
+
+
+        //check if territories have different owners
+        Territory[] map = board.getTerritory();
+        for(int i = 0; i<map.length; i++)
+        {
+            if(map[i].getPlayer() == p1){
+                p1.move();
+            }
+            player.attack;
+
+        }
+
+        //check if territory that we are attacking from have +1 troop
+        if(movement.getSoldiers() <= 1){
+            //choose another territory to attack from
+        }
+        
+        player.attack();
+
+        //check if the territory belongs to the same player
+        //needs to get the territory that you want to move your troops to, and check if its allowed
+        //check if there is +1 troop
+        player.reinforce();
     }
 
-    public void moveSoldiers(int amount, Movement m) {
 
-        p2.getCurrentTerritory().setSoldiersOut(amount);
+    @Override
+    public int putTroops(int amount, Territory territory) {
+        return 0;
+    }
 
-        moveToTerritory(p2, m);
+    @Override
+    public void increment(Player player) {
+        board.increment(player);
+    }
 
-        p2.getCurrentTerritory().setSoldiersIn(amount);
+    @Override
+    public int limitBoardCol() {
+        return 0;
+    }
+
+    @Override
+    public int limitBoardRow() {
+        return 0;
+    }
+
+    @Override
+    public void battle(Territory territoryAttack, Territory territoryDefend) {
+        board.battle(territoryAttack, territoryDefend);
 
     }
 
+    @Override
+    public void changePlayerTerritory(Player player, Territory territory) {
+
+    }
+
+    @Override
+    public boolean victory() {
+        return false;
+    }
 
 }

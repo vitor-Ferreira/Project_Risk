04b529400c35dab1f72794762f0d9c07caaabeec
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-10-05 16:20:38 +0100

    completed the skeleton of Player

--- src/org/academiadecodigo/variachis/risk_project/Player.java
+++ src/org/academiadecodigo/variachis/risk_project/Player.java
@@ -3,31 +3,66 @@ package org.academiadecodigo.variachis.risk_project;
 public class Player {
 
     private String color;
-    private int soldiers;
-    private Territory currentTerritory;
-    private int numberTerritories;
+    private int soldiersOnTerritory;
+    private int totalSoldiers;
+    private Territory initialTerritory;
+    private Territory currentTerritories;
+    private int numberOfTerritories;
 
-    public Player(String color, int soldiers, Territory random) {
+    public Player(String color, int initialSoldiers, Territory random) {
+
         this.color = color;
-        this.soldiers = soldiers;
-        currentTerritory = random;
-        numberTerritories = 1;
-        currentTerritory.setSoldiersIn(soldiers);
+        this.totalSoldiers = initialSoldiers;
+        this.initialTerritory = random;
+        this.initialTerritory.setSoldiersIn(initialSoldiers);
+        this.numberOfTerritories = 1;
     }
 
-    public void setCurrentTerritory(Territory t) {
-        currentTerritory = t;
+    public int checkSoldiersOnTerritory(Territory territory) {
 
+
+    }
+
+    public int getTotalSoldiers() {
+
+        return totalSoldiers;
     }
 
     public Territory getInicialTerritory() {
-        return currentTerritory;
+
+        return initialTerritory;
     }
 
 
-    public Territory getCurrentTerritory() {
-        return currentTerritory;
+    public Territory getCurrentTerritories() {
+
+        return currentTerritories;
     }
 
+    public void addCurrentTerritory(Territory territory) { //add newly conquered terr. to list
 
+        currentTerritories = territory; //
+        numberOfTerritories += 1;
+
+    }
+
+    public int getNumberTerritories() {
+
+        return numberOfTerritories;
+    }
+
+    public Territory move(Territory territory, int soldiers) {
+
+
+    }
+
+    public Territory attack (Territory territory, int soldiers) {
+
+
+    }
+
+    public Territory reinforce(Territory fromTerritory, Territory toTerritory, int soldiers) {
+
+
+    }
 }

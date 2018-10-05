dbe32243e4e9e6f9e90892ea2ca616005a6e9758
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-10-05 17:48:19 +0100

    0.01% more completed

--- src/org/academiadecodigo/variachis/risk_project/Player.java
+++ src/org/academiadecodigo/variachis/risk_project/Player.java
@@ -3,17 +3,17 @@ package org.academiadecodigo.variachis.risk_project;
 public class Player {
 
     private String color;
-    private int soldiersOnTerritory;
+    //private int soldiersOnTerritory;
     private int totalSoldiers;
     private Territory initialTerritory;
     private Territory currentTerritories;
     private int numberOfTerritories;
 
-    public Player(String color, int initialSoldiers, Territory random) {
+    public Player(String color, int initialSoldiers, Territory randomInitialTerritory) {
 
         this.color = color;
         this.totalSoldiers = initialSoldiers;
-        this.initialTerritory = random;
+        this.initialTerritory = randomInitialTerritory;
         this.initialTerritory.setSoldiersIn(initialSoldiers);
         this.numberOfTerritories = 1;
     }
@@ -28,20 +28,24 @@ public class Player {
         return totalSoldiers;
     }
 
+    public void setTotalSoldiers(int addTotalSoldiers) {
+
+        this.totalSoldiers += addTotalSoldiers;
+    }
+
     public Territory getInitialTerritory() {
 
         return initialTerritory;
     }
-
 
     public Territory getCurrentTerritories() {
 
         return currentTerritories;
     }
 
-    public void addCurrentTerritory(Territory territory) { //add newly conquered terr. to list
+    public void addConqueredTerritory(Territory territory) { //add newly conquered terr. to list
 
-        currentTerritories = territory; //
+         = territory; // <--
         numberOfTerritories += 1;
 
     }
@@ -51,22 +55,30 @@ public class Player {
         return numberOfTerritories;
     }
 
-    public Territory move(Territory territory, int soldiers) {
+    ///// ----- ///// ----- ///// ----- /////
 
-        territory.setSoldiersIn(soldiers);
-
-    }
-
-    public Territory attack (Territory territory, int soldiers) {
+    /* public Territory move(Movement movement, Territory territory) { //for now, all soldiers-1 are moved
 
 
 
+        .putSoldiersIn(soldiers); //just "territory" at the beginning doesn't make sense
+
+    } */
+
+    public Territory attack(Territory fromTerritory, Territory toTerritory) {
+
+        
+
+        board.battle;
     }
 
     public Territory reinforce(Territory fromTerritory, Territory toTerritory, int soldiers) {
 
-        fromTerritory.setSoldiersOut(soldiers);
-        toTerritory.setSoldiersIn(soldiers);
+
+
+        .setSoldiersOut(soldiers);
+        .setSoldiersIn(soldiers);
+
 
     }
 }

5763b0cca7eb312c01786b85e343e79a75d49dc4
Miguel Ângelo <miguelangelo_fm@hotmail.com>
2018-10-07 22:08:37 +0100

    added classes Grid and Position. directed simplegraphics as the library to use

---
+++ src/org/academiadecodigo/variachis/risk_project/Grid.java
@@ -0,0 +1,22 @@
+package org.academiadecodigo.variachis.risk_project;
+
+import java.awt.*;
+
+public class Grid {
+
+    private Rectangle rectangle;
+    private int cols;
+    private int rows;
+    private int padding = 10;
+    private int cellSize = 100;
+
+    public Grid(int cols, int rows) {
+        this.cols = cols;
+        this.rows = rows;
+    }
+
+    public void canvas() {
+
+        //Canvas.getInstance(); 
+    }
+}
